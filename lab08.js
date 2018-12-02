4412e71a0b9fb4dd812f8ffe311b2c11f2a3b078
Luigi Alcuaz <luigi.alcuaz0197@gmail.com>
2018-12-01 23:11:25 -0800

    bloop

--- lab08.js
+++ lab08.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
+    // GET A LIST OF 'THINGS' FROM THE SERVER AS HTML DATA
+    $('#features').click(function(e) {
+
+      // don't allow the anchor to visit the link
+      e.preventDefault();
+
+      $.ajax({
+          url: "/topTen.html",
+          dataType: "html",
+          type: "GET",
+          data: { format: "html-list"},
+          success: function(data) {
+              console.log("SUCCESS HTML:", data);
+              $("#content").html(data);
+
+          },
+      });
+  });
 
 
-  
 });
